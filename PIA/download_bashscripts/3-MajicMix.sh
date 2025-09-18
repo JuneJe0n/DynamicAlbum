2e779b67f7826780716ccac05167505696e31d51
jiyoon <jyunejeon@gmail.com>
2025-09-19 02:52:45 +0900

    remove downloading codes

--- PIA/download_bashscripts/3-MajicMix.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -O /data2/jiyoon/pia/models/DreamBooth_LoRA/majicmixRealistic_v5.safetensors https://civitai.com/api/download/models/82446 --content-disposition --no-check-certificate
